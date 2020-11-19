65945676458405ca449e50d319ec925312100852
Matt Hamada <ermacaz@gmail.com>
2020-11-18 20:21:07 -0700

    allow cors

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Newsfeed
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
