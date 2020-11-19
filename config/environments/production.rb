be7f0111c719a5bf17f0689a01ed5dbcf7c956a9
Matt Hamada <ermacaz@gmail.com>
2020-11-18 20:33:14 -0700

    production websockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.web_socket_server_url = "wss://newsfeedapi.ermacaz.com/cable"
-  config.action_cable.allowed_request_origins = ["https://newsfeedapi.ermacaz.com", "https://newsfeed.ermacaz.com"]
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -35,8 +33,13 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://newsfeedapi.ermacaz.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  #
+  #
+  config.web_socket_server_url = "wss://newsfeedapi.ermacaz.com/cable"
+  config.action_cable.allowed_request_origins = ["https://newsfeedapi.ermacaz.com", "https://newsfeed.ermacaz.com"]
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
